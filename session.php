944e86b38a6a23d020bac9836826d2d60664601d
Sushant Verma <32956593+Sush7597@users.noreply.github.com>
2018-08-26 17:32:39 +0530

    Update session.php

--- session.php
+++ session.php
@@ -4,7 +4,7 @@ session_start();
 $db = db_open();
 $user_check = $_SESSION['login_user'];
 
-$ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
+$ses_sql = mysqli_query($db,"select id from emp where id = '$user_check' ");
 
 $row = mysqli_fetch_array($ses_sql,$db);
 
